438106906e7d1ac083d9611392b5ed99689bc8d1
Iurii Golskyi <igolskyi@gmail.com>
2021-11-07 23:14:42 +0200

    add prettier, update to node 16

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,35 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    semi: ['error', 'never'],
-    camelcase: 'off',
-    'consistent-return': 'off',
-    'arrow-parens': ['error', 'as-needed'],
-    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
-    'no-debugger': 'error',
-    'max-len': [
-      'error',
-      {
-        code: 80,
-        ignoreComments: true,
-        ignoreRegExpLiterals: true,
-        ignoreTrailingComments: true,
-        ignoreTemplateLiterals: true,
-        ignoreStrings: true,
-        ignoreUrls: true,
-      },
-    ],
-  },
-}
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    extends: ['airbnb-base', 'plugin:prettier/recommended'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2021,
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        semi: ['error', 'always'],
+        'consistent-return': 'off',
+        'arrow-parens': ['error', 'as-needed'],
+        'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+        'no-debugger': 'error',
+        'max-len': [
+            'error',
+            {
+                code: 80,
+                ignoreComments: true,
+                ignoreRegExpLiterals: true,
+                ignoreTrailingComments: true,
+                ignoreTemplateLiterals: true,
+                ignoreStrings: true,
+                ignoreUrls: true,
+            },
+        ],
+    },
+};
